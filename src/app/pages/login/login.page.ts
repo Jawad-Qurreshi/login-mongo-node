ea6e5dcd6fa09f3774003e1a57fe3bda9b0e2e0d
jawad-Qurreshi <jmqureshi589@gmail.com>
2019-12-25 18:02:14 +0500

    make it better again

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router'
 import { FormGroup } from '@angular/forms';
 import { UserService } from 'src/app/sdk/custom/user.service' ;
+import { ToastService } from './../../services/toast.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -12,6 +14,7 @@ export class LoginPage implements OnInit {
   constructor(private router: Router,private formBuilder: FormBuilder,private userService:UserService ) {}
   loginForm: FormGroup;
   loading : boolean;
+  private toastService: ToastService
   ngOnInit() {
     this.formInitializer();
   }
@@ -24,22 +27,33 @@ export class LoginPage implements OnInit {
   }
 
   save() {
+    if (this.loginForm.value)
+    {
     const loginData = this.loginForm.value;
     console.log('loginData', loginData);
     // we need to send this data to our node.js server
-
     this.userService.userLogin(loginData).subscribe(
-      data => {
-        console.log('got response from server', data);
+      (data: any) => {
+        if (data.userData){
+          console.log('got response from server', data);
         this.loading = false;
         this.router.navigateByUrl('/home');
+        }else {
+          this.toastService.presentToast('Incorrect username and password.');
+        }
       },
-      error => {
+      (error:any) => {
         this.loading = false;
         console.log('error in login save method', error);
         this.router.navigateByUrl('/register');
       }
     );
+    }
+    else{
+      this.toastService.presentToast(
+        'Please enter username or password.'
+        );
+    }
   }
 
 }
